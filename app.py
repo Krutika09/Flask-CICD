c61a8c89e7900c9d10366d26b92b728b155c5c62
Krutika09 <krutikayawale2002@gmail.com>
2025-07-07 16:13:30 +0530

    new chnages

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello from CI/CD!"
 
+@app.route('/info')
+def info():
+    return "This is info page.."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
